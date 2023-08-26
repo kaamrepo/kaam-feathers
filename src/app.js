917f566f3c0735a9c6cd252d9fad2e71f047ceb8
SidheshLineup <sidheshlineup@gmail.com>
2023-08-26 17:13:38 +0530

    env changes

--- src/app.js
+++ src/app.js
@@ -21,14 +21,11 @@ import { authentication } from './authentication.js'
 import { services } from './services/index.js'
 import { channels } from './channels.js'
 
-
 // firebase notification imports:
 import admin from 'firebase-admin'
 
 // cloudinary image imports:
 import cloudinary from 'cloudinary'
-
-
 
 const app = express(feathers())
 
@@ -60,18 +57,13 @@ app.configure(channels)
 app.use(notFound())
 app.use(errorHandler({ logger }))
 
-
-
-console.log("🍔🍔🍔", app.get('firebase_db_url'))
 // Fcm Notification
 const uid = 'some-uid'
 const additionalClaims = {
   premiumAccount: true
 }
-function jsonConcat(o1, o2)
-{
-  for (let key in o2)
-  {
+function jsonConcat(o1, o2) {
+  for (let key in o2) {
     o1[key] = o2[key]
   }
   return o1
@@ -79,8 +71,7 @@ function jsonConcat(o1, o2)
 let serviceAccountInfo = app.get('firebase_fcm_serive_account')
 let serviceAuthUrl = app.get('firebase_fcm_auth_url')
 let serviceAccount = {}
-if (serviceAuthUrl && Object.keys(serviceAuthUrl).length != 0)
-{
+if (serviceAuthUrl && Object.keys(serviceAuthUrl).length != 0) {
   serviceAccount = jsonConcat(serviceAccount, JSON.parse(serviceAuthUrl))
 }
 serviceAccount = jsonConcat(serviceAccount, JSON.parse(serviceAccountInfo))
@@ -94,7 +85,6 @@ admin
   .auth()
   .createCustomToken(uid, additionalClaims)
   .catch((error) => console.log(error))
-
 
 //cloudnary setup
 cloudinary.config({
