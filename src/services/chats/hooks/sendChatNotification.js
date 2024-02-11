de9f782ea232a627e2e41c7929413089ac4fae85
SidheshLineup <sidheshlineup@gmail.com>
2024-02-12 00:36:33 +0530

    removed console

--- src/services/chats/hooks/sendChatNotification.js
+++ src/services/chats/hooks/sendChatNotification.js
@@ -2,7 +2,6 @@
 // import { jobapplicationPath } from '../../jobapplications/jobapplications.shared.js'
 
 export const sendChatNotification = async (context) => {
-  console.log('I am here')
   //   const employer = await context.app.service(jobapplicationPath).get(context?.result?.employerid)
   //   console.log(employer)
   //   notificationHelper(context.app)
