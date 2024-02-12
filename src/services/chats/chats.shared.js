60f0018951049f319c5c4c71817d7a6225ed3009
SidheshLineup <sidheshlineup@gmail.com>
2024-02-13 01:37:53 +0530

    chatting feature done

--- src/services/chats/chats.shared.js
+++ src/services/chats/chats.shared.js
@@ -1,10 +1,9 @@
-import { COMMON_ENDPOINT } from "../../constant/endpoints.js"
+import { COMMON_ENDPOINT } from '../../constant/endpoints.js'
 
-export const chatPath = `${ COMMON_ENDPOINT }chats`
+export const chatPath = `${COMMON_ENDPOINT}chats`
 export const chatMethods = ['find', 'get', 'create', 'patch', 'remove']
 
-export const chatClient = (client) =>
-{
+export const chatClient = (client) => {
   const connection = client.get('connection')
 
   client.use(chatPath, connection.service(chatPath), {
