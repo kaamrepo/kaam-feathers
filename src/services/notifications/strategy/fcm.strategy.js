0bd0ba37b110a70992b453aeeba7c5858b0d8333
SidheshLineup <sidheshlineup@gmail.com>
2024-05-05 03:35:32 +0530

    fcm notification added

--- src/services/notifications/strategy/fcm.strategy.js
+++ src/services/notifications/strategy/fcm.strategy.js
@@ -1,15 +1,15 @@
 import admin from 'firebase-admin'
 
 export class FCMNotification {
-    sendNotification(payload){
-
-        const notificationBody = {
-            tokens:payload.recepient,
-            notification: payload.notification
-        }
-        return admin.messaging().sendEachForMulticast({
-            ...notificationBody,
-            android: { priority: 'high' },
-          });
+  sendNotification(payload) {
+    const notificationBody = {
+      tokens: payload.recepient,
+      notification: payload.notification
     }
+    return admin.messaging().sendEachForMulticast({
+      ...notificationBody,
+      data: { priority: 'high' },
+      android: { priority: 'high' }
+    })
+  }
 }
