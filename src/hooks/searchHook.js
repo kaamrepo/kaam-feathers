69b83f3014dc00fd6c93cefd131365b727d77340
kaamrepo <kaamappofficial@gmail.com>
2024-06-07 17:08:08 +0530

    udpated hook

--- src/hooks/searchHook.js
+++ src/hooks/searchHook.js
@@ -1,32 +1,24 @@
-import { ObjectID } from 'mongodb';
-
-export const searchHook = async (hook) => {
-  let query = hook.params.query;
-  console.log('in the SEARCH hook query Entered', query);
-
+import { ObjectId } from 'mongodb'
+export const searchHook = (hook) => async (hook) => {
+  let query = hook.params.query
+  console.log('in the SEARCH hook query Entered', query)
   if (query && query !== undefined) {
-    Object.keys(query).forEach((key) => {
+    Object.keys(hook.params.query).forEach((key) => {
       switch (key) {
         case 'categories':
           if (Array.isArray(query['categories'])) {
-            query['tags'] = query['categories'].map((category) => new ObjectID(category));
+            query['tags'] = { $in: query['categories'].map((category) => new ObjectId(category)) }
           } else if (query['categories']) {
-            query['tags'] = [new ObjectID(query['categories'])];
+            query['tags'] = { $in: [ObjectId(query['categories'])] }
           }
-          delete query['categories'];
+          delete query['categories']
           break;
         default:
-          break;
+          break
       }
-    });
-
-    // Convert to the required query format
-    if (query['tags']) {
-      query['tags'] = { $in: query['tags'] };
-    }
-
-    hook.params.query = query;
-    console.log('hoook.params.query at LAST in search', hook.params.query);
+    })
+    hook.params.query = query
+    console.log('hoook.params.query at LAST in search', hook.params.query)
   }
-  return hook;
+  return hook
 }
